80741d9b1f71057fb11e60df85ca6d4a6a20cd36
laclustr <99510680+laclustr@users.noreply.github.com>
2025-02-02 20:07:47 -0500

    need to figure out dfs algorithm

--- Problem Sets/CSP-Pset3/games.py
+++ Problem Sets/CSP-Pset3/games.py
@@ -337,9 +337,9 @@ def hitori_solved(puzzle):
 		#Checks no duplicates by column
 		for i in range(len(curr_col)):
 			for j in range(len(curr_col)):
-				if i != j and curr_col[i] == curr_col[j] and curr_col != 0:
+				if i != j and curr_col[i] == curr_col[j] and curr_col == 0:
 					return False
-	return True
+	return hitori_connected(puzzle)
 
 #Problem 7
 def hitori(puzzle):
@@ -364,17 +364,6 @@ def hitori(puzzle):
 		if hitori_solved(puzzle):
 			print("You Win!")
 			break
-puzzle = [
-[4, 8, 0, 6, 3, 2, 0, 7],
-[3, 6, 7, 2, 1, 0, 5, 4],
-[0, 3, 4, 0, 2, 8, 6, 1],
-[4, 1, 0, 5, 7, 0, 3, 0],
-[7, 0, 3, 0, 8, 5, 1, 2],
-[0, 5, 6, 7, 0, 1, 8, 0],
-[6, 0, 2, 3, 5, 4, 7, 8],
-[8, 7, 1, 4, 0, 3, 0, 6]
-]
-hitori(puzzle)
 
 
 
