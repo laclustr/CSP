9d94efca0bf791b2c431f49df1006c2a16985dd9
Grant Vance <gant.a.vance@gmail.com>
2025-02-22 09:42:52 -0500

    Remove audio_csp dependency library

--- Problem Sets/CSP-Pset2/audio_csp/audio_csp.py
+++ Problem Sets/CSP-Pset2/audio_csp/audio_csp.py
@@ -4,39 +4,38 @@ import os
 import subprocess
 
 ### START OF DEPENDENCY JUNK ###
-
-if not os.path.exists("assets"):
-  os.mkdir("assets")
+# if not os.path.exists("assets"):
+#   os.mkdir("assets")
   
-sys.path.append("assets")
+# sys.path.append("assets")
 
-def is_package_installed(package_name, target_folder):
-  """ Check if package_name is installed to a specific target_folder """
+# def is_package_installed(package_name, target_folder):
+#   """ Check if package_name is installed to a specific target_folder """
   
-  package_folder = os.path.join(target_folder, package_name)
+#   package_folder = os.path.join(target_folder, package_name)
 
-  return os.path.exists(package_folder)
+#   return os.path.exists(package_folder)
 
-def install_package_to_folder(package_name, target_folder):
-  """ Installs package_name to target_folder using pip/pip3 """
-  if not is_package_installed(package_name, target_folder):
-    print(f"Installing: {package_name} to {target_folder} folder...")
-    try:
-        with open(os.devnull, "w") as devnull:
-          try:
-            if os.name == "nt":
-              subprocess.check_call(['pip', 'install', '--target', target_folder, package_name], stdout=devnull, stderr=devnull)
-            else:
-              subprocess.check_call(['pip3', 'install', '--target', target_folder, package_name], stdout=devnull, stderr=devnull)
-          except subprocess.CalledProcessError:
-            raise Exception(f"pip must be installed, and you must have an internet connection. Verify with pip --version or pip3 --version on mac/Linux")
+# def install_package_to_folder(package_name, target_folder):
+#   """ Installs package_name to target_folder using pip/pip3 """
+#   if not is_package_installed(package_name, target_folder):
+#     print(f"Installing: {package_name} to {target_folder} folder...")
+#     try:
+#         with open(os.devnull, "w") as devnull:
+#           try:
+#             if os.name == "nt":
+#               subprocess.check_call(['pip', 'install', '--target', target_folder, package_name], stdout=devnull, stderr=devnull)
+#             else:
+#               subprocess.check_call(['pip3', 'install', '--target', target_folder, package_name], stdout=devnull, stderr=devnull)
+#           except subprocess.CalledProcessError:
+#             raise Exception(f"pip must be installed, and you must have an internet connection. Verify with pip --version or pip3 --version on mac/Linux")
 
-    except subprocess.CalledProcessError:
-      raise Exception(f"Error installing {package_name}")
+#     except subprocess.CalledProcessError:
+#       raise Exception(f"Error installing {package_name}")
 
 
-install_package_to_folder("numpy", "assets")
-import numpy as np
+# install_package_to_folder("numpy", "assets")
+# import numpy as np
 
 ### END OF DEPENDENCY JUNK ###
 
